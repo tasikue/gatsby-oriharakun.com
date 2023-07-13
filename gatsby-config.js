b0199f6d5b1de5145e98bc9cf186b8db3f2a2ab4
tasikue <tasikue@gmail.com>
2023-07-13 22:25:29 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `oriharakun.com`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
